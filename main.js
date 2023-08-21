98e8aed436101c9c83b4487c18174a3a5e71ae16
Jekaterina-1991 <jekaterina.kljukina@gmail.com>
2023-08-21 16:51:17 +0200

    added style

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let btn = document.querySelector("#btn");
-let sidebar = document.querySelector("sidebar");
+let sidebar = document.querySelector(".sidebar");
 
 btn.onclick = function () {
   sidebar.classList.toggle("active");
